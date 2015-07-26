fd4812870dc50b703f4cf53be58f930bd6b7f790
Mikko Rönkkö <mikko.ronkko@aalto.fi>
2015-07-26 10:58:22 +0300

    A helper file for installing all dependencies.

---
+++ installDependencies.R
@@ -0,0 +1,12 @@
+# Matrixpls dependencies
+install.packages(c("assertive",
+"matrixcalc",
+"roxygen2",
+"devtools",
+"psych",
+"lavaan",
+"plspm",
+"simsem",
+"RUnit",
+"semPLS",
+"boot"), type = "source")
