fcccc286db37a5770b3402c1e251302e19873625
Mikko Rönkkö <mikko.ronkko@aalto.fi>
2016-05-27 10:45:21 +0300

    Channged the wording of some arguments

--- R/matrixpls.postestimation.R
+++ R/matrixpls.postestimation.R
@@ -575,9 +575,9 @@ predict.matrixpls <- function(object, newdata, ...){
   W <- attr(object,"W")
   inner <- attr(object,"inner")
   
-  reflective <- nativeModel$reflective
-  reflective[which(reflective==1)] <- object[grep("=~",names(object))]
   
+  reflective <- attr(object,"reflective")
+
   # Reorder the variables in newdata
   data <- NULL
   
