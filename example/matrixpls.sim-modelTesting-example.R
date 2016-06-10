d750b3bf034de35e09b39f52b03ca6a6e482dd0c
FloSchuberth <florian.schuberth@uni-wuerzburg.de>
2016-06-10 15:02:55 +0200

    Changed some comments and add results

--- example/matrixpls.sim-modelTesting-example.R
+++ example/matrixpls.sim-modelTesting-example.R
@@ -1,5 +1,5 @@
 library(matrixpls) # package to estimate VBSEM
-library(boot) # packages for bootstrap 
+library(boot) # packages for bootstrap
 library(simsem)  # package for simulation of data sets
 library(lavaan)  # package for latent variable analysis
 
@@ -22,53 +22,60 @@ y4~~.64*y4; y5~~.51*y5; y6~~.36*y6
 
 #structural model
 XI2~.6*XI1
-XI3~.6*XI2 +.0*XI1 
+XI3~.6*XI2 +.0*XI1
 
-# (Co-)Variance of the common factors 
+# (Co-)Variance of the common factors
 XI1~~1*XI1
 XI2~~0.64*XI2
 XI3~~0.64*XI3
 "
 
-ndraws=1000        # number of draws
-nobservations=300  # number of observations
-multi= FALSE       # parallel computing TRUE or FALSE
+ndraws = 1000        # number of draws
+nobservations = 1200  # number of observations
+multi = FALSE       # parallel computing TRUE or FALSE
 
 # Create data sets
-datmat.factor <- sim(nRep = ndraws, model = MODEL, n = nobservations, dataOnly = TRUE, multicore =F)    
+datmat.factor <-  sim(nRep = ndraws,
+                      model = MODEL,
+                      n = nobservations,
+                      dataOnly = TRUE,
+                      multicore = F)
 
 # Estimate the model with PLSc (Dijkstra & Henseler (2015)) using the data sets created earlier
-sim.res <- matrixpls.sim(model = MODEL, 
-                                 disattenuate = TRUE,
-                                reliabilities = reliability.weightLoadingProduct,
-                                parametersReflective = estimator.plscLoadings,
-                                rawData = datmat.factor,
-                                multicore = multi,
-                                boot.R = FALSE)
+sim.res <- matrixpls.sim(model = MODEL,
+                         disattenuate = TRUE,
+                         reliabilities = reliability.weightLoadingProduct,
+                         parametersReflective = estimator.plscLoadings,
+                         rawData = datmat.factor,
+                         multicore = multi,
+                         boot.R = FALSE)
 
 summary(sim.res)
 
-# save estimates 
-plsc.res <- sim.res@extraOut # same as I get 
+# save estimates
+plsc.res <- sim.res@extraOut
 
 # Calculate the model implied correlation matrix for every estimation
-Sighats <- lapply(plsc.res,function(x) fitted(x))  
+Sighats <- lapply(plsc.res, function(x)
+  fitted(x))
 
-# Function that calculates the squared Euclidean distance between the empirical and the model implied correlation matrix for every estimation
-calculateTestStat <- function(matrixpls.res){
-  S <- attr(matrixpls.res,"S")
+# Function that calculates the squared Euclidean distance between the empirical and
+# the model implied correlation matrix for every estimation
+calculateTestStat <- function(matrixpls.res) {
+  S <- attr(matrixpls.res, "S")
   Sigma <- fitted(matrixpls.res)
-   sum((S-Sigma)[lower.tri(S, diag = FALSE)]^2) 
+  sum((S - Sigma)[lower.tri(S, diag = FALSE)] ^ 2)
 }
 
 # calculate the distances for the initial data sets
 teststat <- lapply(plsc.res, calculateTestStat)
 
 
-# Function that tranforms the data sets in the way proposed by Yuan & Hayashi (2003), see also Dijkstra & Henseler (2015)
+# Function that tranforms the data sets in the way proposed by Yuan & Hayashi (2003), 
+# see also Dijkstra & Henseler (2015)
 scaleDataSet <- function(data,Sig){    
   S <- cor(data)
-  # singular value decomposition
+  # singular value decomposition of S and Sigma
   Ssvd=svd(S); Sigsvd=svd(Sig)
   dS=Ssvd$d; dSig=Sigsvd$d
   uS=Ssvd$u; uSig=Sigsvd$u
@@ -87,26 +94,25 @@ scaleDataSet <- function(data,Sig){
 
 # Create transformed data sets
 scaleddata <- mapply(scaleDataSet, 
-                  data=datmat.factor,
-                  Sig=Sighats,
-                  SIMPLIFY = FALSE)     # I get the same transformed data sets
+                     data=datmat.factor,
+                     Sig=Sighats,
+                     SIMPLIFY = FALSE)
 
 
 # Calculate the empirical reference distribution
 sim.res2 <- matrixpls.sim(model = MODEL, disattenuate = TRUE,
-                         reliabilities = reliability.weightLoadingProduct,
-                         parametersReflective = estimator.plscLoadings,
-                         rawData = scaleddata,
-                         multicore = F,
-                         boot.R = 500,
-                         # Calculate the test statistic for each bootstrap replication
-                         extraFun = calculateTestStat)
+                          reliabilities = reliability.weightLoadingProduct,
+                          parametersReflective = estimator.plscLoadings,
+                          rawData = scaleddata,
+                          multicore = F,
+                          boot.R = 500,
+                          # Calculate the test statistic for each bootstrap replication
+                          extraFun = calculateTestStat)
 
 
 rejection=mapply(function(x,y){
   # The last column contains the test statistic
   crit=quantile(x$t[,ncol(x$t)],0.9) # critical value for comparison
- # print(paste(y,crit))
   return(y>crit)      
 },x=sim.res2@extraOut,y=teststat)
 
@@ -115,14 +121,25 @@ rejection=mapply(function(x,y){
 # TRUE: rejection
 table(rejection)
 
+# Results for different sample sizes:
+# n=300
+# FALSE  TRUE 
+#  914    86 
 
+# n=600
+# FALSE  TRUE 
+# 921    79 
 
-# The results support the findings by Dijkstra & Henseler (2015) (Table 7), although the results for the model considered are slightly better.
- 
+# n=1200
+# FALSE  TRUE 
+#  901    99
+
+# The results support the findings by Dijkstra & Henseler (2015) (Table 7) 
+
 # References:
 #Yuan, Ke-Hai, and Kentaro Hayashi. "Bootstrap approach to inference and power analysis based on three test statistics for covariance structure models." British Journal of Mathematical and Statistical Psychology 56.1 (2003): 93-110.
 
-#Dijkstra, Theo K., and J?rg Henseler. "Consistent and asymptotically normal PLS estimators for linear structural equations." Computational statistics & data analysis 81 (2015): 10-23.
+#Dijkstra, Theo K., and Henseler, Joerg. "Consistent and asymptotically normal PLS estimators for linear structural equations." Computational statistics & data analysis 81 (2015): 10-23.
 
-#Henseler, J?rg, and Sarstedt, Marko. "Goodness-of-fit indices for partial least squares path modeling." Computational Statistics 28.2 (2013): 565-580.
+#Henseler, Joerg, and Sarstedt, Marko. "Goodness-of-fit indices for partial least squares path modeling." Computational Statistics 28.2 (2013): 565-580.
 
