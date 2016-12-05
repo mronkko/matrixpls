87f490c2776de008f6de03edba662489c6acfa85
Mikko Rönkkö <mikko.ronkko@aalto.fi>
2016-12-05 11:59:39 +0200

    Streamlined teh example and fixed a scaling issue

--- example/matrixpls.sim-modelTesting-example.R
+++ example/matrixpls.sim-modelTesting-example.R
@@ -1,8 +1,11 @@
-library(matrixpls) # package to estimate VBSEM
+# This example contains code cotributed by Florian Schuberth (florian.schuberth@uni-wuerzburg.de)
+
+library(matrixpls)
 library(boot) # packages for bootstrap
 library(simsem)  # package for simulation of data sets
 library(lavaan)  # package for latent variable analysis
-
+library(expm) # matrix square roots
+ 
 set.seed(2015)
 
 ### Data generation  #####
@@ -30,21 +33,22 @@ XI2~~0.64*XI2
 XI3~~0.64*XI3
 "
 
-ndraws = 1000        # number of draws
-nobservations = 1200  # number of observations
-multi = FALSE       # parallel computing TRUE or FALSE
+ndraws <- 1000        # number of draws
+nobservations <- 1200  # number of observations
+multi <- TRUE       # parallel computing TRUE or FALSE
 
 # Create data sets
 datmat.factor <-  sim(nRep = ndraws,
                       model = MODEL,
                       n = nobservations,
                       dataOnly = TRUE,
-                      multicore = F)
+                      multicore = multi)
 
 # Estimate the model with PLSc (Dijkstra & Henseler (2015)) using the data sets created earlier
+
 sim.res <- matrixpls.sim(model = MODEL,
                          disattenuate = TRUE,
-                         reliabilities = reliability.weightLoadingProduct,
+                         reliabilities = reliabilityEstim.weightLoadingProduct,
                          parametersReflective = estimator.plscLoadings,
                          rawData = datmat.factor,
                          multicore = multi,
@@ -52,12 +56,8 @@ sim.res <- matrixpls.sim(model = MODEL,
 
 summary(sim.res)
 
-# save estimates
+# Extract estimates
 plsc.res <- sim.res@extraOut
-
-# Calculate the model implied correlation matrix for every estimation
-Sighats <- lapply(plsc.res, function(x)
-  fitted(x))
 
 # Function that calculates the squared Euclidean distance between the empirical and
 # the model implied correlation matrix for every estimation
@@ -73,21 +73,38 @@ teststat <- lapply(plsc.res, calculateTestStat)
 
 # Function that tranforms the data sets in the way proposed by Yuan & Hayashi (2003), 
 # see also Dijkstra & Henseler (2015)
-scaleDataSet <- function(data,Sig){    
-  S <- cor(data)
-  # singular value decomposition of S and Sigma
-  Ssvd=svd(S); Sigsvd=svd(Sig)
-  dS=Ssvd$d; dSig=Sigsvd$d
-  uS=Ssvd$u; uSig=Sigsvd$u
-  vS=Ssvd$v; vSig=Sigsvd$v
-  #S^{-1/2}
-  S_half=uS%*%(diag(dS^(-1/2)))%*%t(vS)
-  # Sigma_hat^(1/2)
-  Sig_half=uSig%*%(diag(dSig^(1/2)))%*%t(vSig)
-  # scale factor Sig_hat^(1/2)%*%S^(-1/2)
-  sf=S_half%*%Sig_half
+
+# The bootstrap approach only works correctly if  the empirical variance-covariance matrix of the
+# transformed data set (Z) equals the model-implied covariance matrix, cov(Z)=Sigma_hat 
+# (Beran & Srivastava, 1985). Therefore, we need to check that that the data set X corresponds to
+# S and Sigma_hat. If X is not scaled to means zero and unit variances, S and Sigma_hat need to be
+# covariance matrices, otherwise, if X is scaled, S and Sigma_hat need to be correlation matrices.
+#
+# Beran, R. & Srivastava, M. S.
+# Bootstrap tests and confidence regions for functions of a covariance matrix
+# The Annals of Statistics, JSTOR, 1985, 13, 95-115  
+#
+# Because PLS weights are not scale free, that is, they depend on the scale of the raw data,
+# the function will check that S matches the covariance matrix of the data and automatically
+# rescale the dataset
+
+scaleDataSet <- function(data, matrixpls.res) {
   
-  sdata <- as.matrix(data)%*%sf
+  S   <- attr(matrixpls.res, "S")
+  Sig <- fitted(matrixpls.res)
+  
+  ## S^{-1/2}
+  S_half   <- solve(expm::sqrtm(S))
+  Sig_half <- expm::sqrtm(Sig)
+  
+  ## Scale factor Sig_hat^(1/2)%*%S^(-1/2)
+  sf <- S_half %*% Sig_half  
+  
+  sdata <- as.matrix(data)
+  
+  if(any(cov(data) != S)) {sdata <- scale(sdata)}
+
+  sdata <- sdata %*% sf
   colnames(sdata) <- colnames(data)
   sdata
 }
@@ -95,13 +112,13 @@ scaleDataSet <- function(data,Sig){
 # Create transformed data sets
 scaleddata <- mapply(scaleDataSet, 
                      data=datmat.factor,
-                     Sig=Sighats,
+                     matrixpls.res=plsc.res,
                      SIMPLIFY = FALSE)
 
 
 # Calculate the empirical reference distribution
 sim.res2 <- matrixpls.sim(model = MODEL, disattenuate = TRUE,
-                          reliabilities = reliability.weightLoadingProduct,
+                          reliabilities = reliabilityEstim.weightLoadingProduct,
                           parametersReflective = estimator.plscLoadings,
                           rawData = scaleddata,
                           multicore = F,
@@ -117,29 +134,8 @@ rejection=mapply(function(x,y){
 },x=sim.res2@extraOut,y=teststat)
 
 # Present results
-#FALSE: non-rejection
+# FALSE: non-rejection
 # TRUE: rejection
+
 table(rejection)
-
-# Results for different sample sizes:
-# n=300
-# FALSE  TRUE 
-#  914    86 
-
-# n=600
-# FALSE  TRUE 
-# 921    79 
-
-# n=1200
-# FALSE  TRUE 
-#  901    99
-
-# The results support the findings by Dijkstra & Henseler (2015) (Table 7) 
-
-# References:
-#Yuan, Ke-Hai, and Kentaro Hayashi. "Bootstrap approach to inference and power analysis based on three test statistics for covariance structure models." British Journal of Mathematical and Statistical Psychology 56.1 (2003): 93-110.
-
-#Dijkstra, Theo K., and Henseler, Joerg. "Consistent and asymptotically normal PLS estimators for linear structural equations." Computational statistics & data analysis 81 (2015): 10-23.
-
-#Henseler, Joerg, and Sarstedt, Marko. "Goodness-of-fit indices for partial least squares path modeling." Computational Statistics 28.2 (2013): 565-580.
 
