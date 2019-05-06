1cfe82186e4cc77cfc5e81bd4b77d75bf677243c
Mikko Rönkkö <mikko.ronkko@jyu.fi>
2019-05-06 09:18:52 +0200

    Added the option to predict composites scores and refactored.

--- R/matrixpls.predict.R
+++ R/matrixpls.predict.R
@@ -10,13 +10,17 @@
 #'
 #'@param newData A data frame or a matrix containing data used for prediction.
 #'
-#'@param predictionType "exogenous" (default) predicts indicators from exogenous composites. "redundancy"
-#'and "communality" are alternative strategies described by Chin (2010).
+#'@param predictionType "exogenous" (default) predicts indicators from exogenous 
+#'composites. "redundancy" and "communality" are alternative strategies 
+#'described by Chin (2010). "composites" returns the composites calculated 
+#'by multiplying the data with the weight matrix.
 #'
-#'@param means A vector of means of the original data used to calculate intercepts for the
-#'linear prediciton equations. If not provided, calculated from the new data or assumed zero.
+#'@param means A vector of means of the original data used to calculate 
+#'intercepts for the linear prediciton equations. If not provided, calculated
+#'from the new data or assumed zero.
 #'
-#'@return a matrix of predicted values for reflective indicators of endogenous latent variables.
+#'@return a matrix of predicted values for reflective indicators of endogenous
+#' latent variables or weighted composites of the indicators.
 #'
 #'@references
 #'
@@ -25,7 +29,8 @@
 #'Chin, W. W. (2010). How to write up and report PLS analyses. In V. Esposito Vinzi, W. W. Chin, J.
 #'Henseler, & H. Wang (Eds.), Handbook of partial least squares (pp. 655–690). Berlin Heidelberg: Springer.
 #'
-#'@export
+#'@example example/matrixpls-example.R
+#'@example example/matrixpls.prediction-basicExample.R
 #'
 #'@method predict matrixpls
 #'
@@ -33,16 +38,15 @@
 
 
 predict.matrixpls <- function(object, newData, 
-                              predictionType = "exogenous",
+                              predictionType = c("exogenous","redundancy",
+                                                 "communality","composites"),
                               means = NULL, ...){
+  
+  predictionType <- match.arg(predictionType)
   
   nativeModel <- attr(object,"model")
   exog <- rowSums(nativeModel$inner)==0
   W <- attr(object,"W")
-  inner <- attr(object,"inner")
-  
-  
-  reflective <- attr(object,"reflective")
   
   # Reorder the variables in newData
   data <- matrix(NA, nrow(newData), 0)
@@ -58,44 +62,61 @@ predict.matrixpls <- function(object, newData,
   
   colnames(data) <- colnames(W)
   
-  if(is.null(means)){
-    means <- apply(data,2,mean,na.rm = TRUE)
-    means[is.nan(means)] <- 0
+  
+  # Prediction of the composite scores
+  
+  if(predictionType == "composites"){
+    browser()
+    pred <- data %*% t(W)
   }
+  
+  # Prediction of the indicators using the three different techniques
+  
   else{
-    means <- means[colnames(data)]
+    
+    inner <- attr(object,"inner")
+    reflective <- attr(object,"reflective")
+    
+    if(is.null(means)){
+      means <- apply(data,2,mean,na.rm = TRUE)
+      means[is.nan(means)] <- 0
+    }
+    else{
+      means <- means[colnames(data)]
+    }
+    
+    # Calculate prediction matrix 
+    
+    
+    if(predictionType == "exogenous"){
+      
+      beta <- diag(exog)
+      
+      # Predict endog LVs using reduced form equations
+      Beta <- inner[! exog, ! exog]
+      Gamma <- inner[! exog, exog]
+      
+      beta[! exog, exog] <- t(Gamma) %*% solve(diag(nrow(Beta))- t(Beta))
+      
+    } else if(predictionType == "redundancy"){
+      
+      beta <- inner
+      diag(beta)[exog]<-1
+      
+    } else if(predictionType == "communality"){
+      
+      beta <- diag(nrow(inner))
+      
+    } else stop('predictionType must be "exogenous", "redundancy", "communality", or "composites"')
+    
+    predictionMatrix <- t(W) %*% t(beta) %*% t(reflective)
+    
+    intercepts <- means - means %*% predictionMatrix
+    
+    # Do the predictions
+    pred <- data %*% predictionMatrix
+    pred <- sweep(pred, 2, intercepts, "+")
   }
   
-  # Calculate prediction matrix 
-  
-  
-  if(predictionType == "exogenous"){
-    
-    beta <- diag(exog)
-    
-    # Predict endog LVs using reduced form equations
-    Beta <- inner[! exog, ! exog]
-    Gamma <- inner[! exog, exog]
-    
-    beta[! exog, exog] <- t(Gamma) %*% solve(diag(nrow(Beta))- t(Beta))
-    
-  } else if(predictionType == "redundancy"){
-    
-    beta <- inner
-    diag(beta)[exog]<-1
-    
-  } else if(predictionType == "communality"){
-    
-    beta <- diag(nrow(inner))
-    
-  } else stop('predictionType must be "exogenous", "redundancy", or "communality"')
-  
-  predictionMatrix <- t(W) %*% t(beta) %*% t(reflective)
-  
-  intercepts <- means - means %*% predictionMatrix
-  
-  # Do the predictions
-  pred <- data %*% predictionMatrix
-  sweep(pred, 2, intercepts, "+")
-
+  pred
 }
